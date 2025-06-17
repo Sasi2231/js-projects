edc0ea3c21fc504bba5f73eeeda25dd185a8be90
sasikumar <sasi2397677@gmail.com>
2025-06-17 16:40:57 +0530

    updated version

--- guess/index.js
+++ guess/index.js
@@ -1,31 +1,17 @@
-const minim = 50;
-const maxim = 100;
-const num = Math.floor(Math.random() * (maxim - minim)+1)+minim
-let running = true;
-let attempts = 0;
-let guess ;  
-let result ;
-function strguess(){
- while(running){
-    let guess = document.getElementById("myGuess").Value
-    guess = Number(guess)
-    if(guess < minim || guess > maxim){
-        result.textContent = `enter between &{maxim} and &{minim}`
-    }
-    else{
-        attempts++;
-        if(guess > num){
-            result.textContent = 'your guess is too high!'
+const minrange = 50;
+const maxrange = 100;
+const randomguess = Math.floor(Math.random() * (maxrange - minrange)) + minrange
+let result = document.getElementById("result")
 
+function strguess(){
+        const guess = document.getElementById("myGuess").value
+        if (guess > randomguess){
+            result.textContent = "You're guess is too high!"
         }
-        else if(guess < num){
-            result.textContent = 'your guess is too low!'
+        else if(guess < randomguess){
+            result.textContent = "You're guess is too low!"
         }
         else{
-            result.textContent = `you guessed correct! ${num}.it took ${attempts} attempts`
-            running = false
+            result.textContent = "You guessed it correct!"
         }
-    }
-
- }
-}
+  }
